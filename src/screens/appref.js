92d0fb8769af0294b32d1a4f19cb33689bf76e78
sohail9845 <sohail9845@gmail.com>
2020-09-16 11:44:27 +0530

    new

--- src/screens/appref.js
+++ src/screens/appref.js
@@ -6,7 +6,7 @@ class appref extends Component {
     }
     render() {
         const utmsource = "shareapp";
-        const utmmedium = "referral";
+        const utmmedium = "product";
         const utmcontent = this.props.match.params.id;
         const prefix = "https://play.google.com/store/apps/details?id=com.shopsath.com&referrer=";
         const url  = prefix+"utm_source%3D"+utmsource+"%26utm_medium%3D"+utmmedium+"%26utm_content%3D"+utmcontent;
