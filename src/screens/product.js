dd4e145657c0f77eb949f22fbcbfb4f08fd4a4aa
sohail9845 <sohail9845@gmail.com>
2020-10-09 13:21:31 +0530

    new

--- src/screens/product.js
+++ src/screens/product.js
@@ -9,7 +9,7 @@ class product extends Component {
         const sku = this.props.match.params.id;
         const query = new URLSearchParams(this.props.location.search);
         const code = query.get('refer');
-        const prefix = "https://play.app.goo.gl/?link=https://play.google.com/store/apps/details?id=com.contexdo.shopsath&referrer=";
+        const prefix = "https://play.google.com/store/apps/details?id=com.contexdo.shopsath&referrer=";
         const url  = prefix+referrer+"&sku="+sku+"&code="+code;
         window.location.replace(url);
         return null
