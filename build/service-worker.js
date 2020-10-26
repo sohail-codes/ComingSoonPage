a24fb807289e232dbc692a5d6465e43437093009
sohail-codes <sohail9845@gmail.com>
2020-10-26 14:42:26 +0530

    new

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb8a51b832b8e79be34ffd85d14bb520.js"
+  "/precache-manifest.f06044a4916032f736defef54b82e17b.js"
 );
 
 self.addEventListener('message', (event) => {
