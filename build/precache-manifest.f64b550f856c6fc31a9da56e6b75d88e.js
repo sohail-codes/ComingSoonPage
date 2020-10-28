e5260e13f68aeb736c83a6d45ff81325a8e744e4
sohail-codes <sohail9845@gmail.com>
2020-10-28 17:09:42 +0530

    new

---
+++ build/precache-manifest.f64b550f856c6fc31a9da56e6b75d88e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626b85245a9790a014ff5b2ee71b9e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8d0661ec5bc003a4b10",
+    "url": "/static/css/main.7eedc3b4.chunk.css"
+  },
+  {
+    "revision": "2d6c788e494e66470af4",
+    "url": "/static/js/2.85aa93b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85aa93b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d0661ec5bc003a4b10",
+    "url": "/static/js/main.ca930d43.chunk.js"
+  },
+  {
+    "revision": "ef2781023dfe6c1288b6",
+    "url": "/static/js/runtime-main.7f062537.js"
+  },
+  {
+    "revision": "ff54236059107711add7925bb6c106cd",
+    "url": "/static/media/appsharehi.ff542360.jpg"
+  },
+  {
+    "revision": "f4849affd496b3496704725e17863f39",
+    "url": "/static/media/categoryen.f4849aff.jpg"
+  },
+  {
+    "revision": "241c018d9be271d9fc2329627447d52a",
+    "url": "/static/media/categoryhi.241c018d.jpg"
+  },
+  {
+    "revision": "4ce9d7ad4b7dcf0fda48ea7ca9c5f1c9",
+    "url": "/static/media/dealsen.4ce9d7ad.jpg"
+  },
+  {
+    "revision": "fa2b04ddd9a9b133c52d94aba9d95581",
+    "url": "/static/media/dealshi.fa2b04dd.jpg"
+  },
+  {
+    "revision": "9de52a78c616e0e1c048f6676f84eb5c",
+    "url": "/static/media/feeden.9de52a78.jpg"
+  },
+  {
+    "revision": "00026fded11119e304ea223ecef6e424",
+    "url": "/static/media/feedhi.00026fde.jpg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "171f48e31a0c59f824200656557c6a12",
+    "url": "/static/media/insta.171f48e3.png"
+  },
+  {
+    "revision": "e1004653bf8357a82571b993b2d7e451",
+    "url": "/static/media/language.e1004653.jpg"
+  },
+  {
+    "revision": "e9c7859293abaf9fa067756a9aec067e",
+    "url": "/static/media/loaderanimation1.e9c78592.png"
+  },
+  {
+    "revision": "8d6dbe2d2912b85e323f67cd3e311308",
+    "url": "/static/media/producten.8d6dbe2d.jpg"
+  },
+  {
+    "revision": "5d716aca6fe27f0d3597703831f84caf",
+    "url": "/static/media/producthi.5d716aca.jpg"
+  },
+  {
+    "revision": "feb715e42bb4ff2d3961c3f57cc97573",
+    "url": "/static/media/sharechat.feb715e4.png"
+  },
+  {
+    "revision": "f3d88a7cd5be201f4f63c69f8705a4a1",
+    "url": "/static/media/walleten.f3d88a7c.jpg"
+  },
+  {
+    "revision": "597738dc468f7d47e89c8481d57f1322",
+    "url": "/static/media/wallethi.597738dc.jpg"
+  },
+  {
+    "revision": "f70ce90cb617667a3ed07c4c2484a99a",
+    "url": "/static/media/welcomeen.f70ce90c.jpg"
+  },
+  {
+    "revision": "3a598790d283b03ef78efecf548070c8",
+    "url": "/static/media/welcomehi.3a598790.jpg"
+  }
+]);
